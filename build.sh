cee6c499a32bc78ff5cf9985e42c5df18122a1f5
ygidtu <ygidtu@gmail.com>
2019-09-10 21:40:08 +0800

    2019.09.09 fix compile

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 env GOOS=darwin GOARCH=amd64 go build -o nbia_cli_darwin main.go tcia.go download.go
 
-env GOOS=darwin GOARCH=amd64 go build -o nbia_cli_linux_amd64 main.go tcia.go download.go
+env GOOS=linux GOARCH=amd64 go build -o nbia_cli_linux_amd64 main.go tcia.go download.go
 
-env GOOS=darwin GOARCH=amd64 go build -o nbia_cli_win64.exe main.go tcia.go download.go
+env GOOS=windows GOARCH=amd64 go build -o nbia_cli_win64.exe main.go tcia.go download.go
 
